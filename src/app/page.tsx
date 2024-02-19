1ed1a47aaad36fa972ba58b0f32d56b07d5a9584
MALTHE-PC\malth <malthe3@outlook.com>
2024-02-19 21:54:49 +0100

    Add image domains to next.config and update CocktailsModel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import { CocktailsModel } from "@/cocktailsmodel";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import cocktailsData from "@/lib/cocktials.json";
 
 export default function Home() {
   const [cocktails, setCocktails] = useState<CocktailsModel[]>([]);
 
   useEffect(() => {
-    setCocktails(cocktails);
-    console.log(cocktails);
+    setCocktails(cocktailsData.cocktailsData);
+    console.log(cocktailsData.cocktailsData);
   }, []);
 
   return (
@@ -23,20 +24,20 @@ export default function Home() {
                 Welcome to Malthes Cocktails
               </h1>
 
-              <div>
+              {/* <div>
                 {cocktails.map((cocktail, index) => (
                   <div key={index}>
                     <h2>{cocktail.strCategory}</h2>
                     <p>{cocktail.strDrinkAlternate}</p>
                     <Image
-                      src={cocktail.strDrinkThumb}
-                      alt={cocktail.strDrink}
+                      src={cocktail.strDrinkThumb!}
+                      alt={cocktail.strDrink!}
                       width={500}
                       height={300}
                     />
                   </div>
                 ))}
-              </div>
+              </div> */}
 
               <p className="max-w-[600px] mt-6 text-base text-gray-500 md:text-xl dark:text-gray-400 lg:text-base/each lg:mt-4">
                 We craft the finest cocktails for your enjoyment. Let us delight
@@ -69,38 +70,44 @@ export default function Home() {
         </div>
       </div>
 
-      <div
-        id="cocktails"
-        className="grid grid-cols-1 md:grid-cols-3 gap-4 bg-gray-100 py-12 px-4 dark:bg-gray-950 lg:px-6"
-      >
-        <div className="flex flex-col gap-5 ">
-          <div className="space-y-2">
-            <h2 className="text-2xl font-bold tracking-tight sm:text-3xl md:text-4xl">
-              Margarita
-            </h2>
-            <p className="text-gray-500 dark:text-gray-400">
-              The classic Margarita is a perfect balance of sweet, sour, and
-              tequila.
-            </p>
-          </div>
+      <div className="flex justify-center bg-gray-100 py-12 dark:bg-gray-950 lg:px-6">
+        <div
+          id="cocktails"
+          className="grid grid-cols-1 md:grid-cols-3 gap-32 "
+        >
+          {cocktails.map((cocktail) => (
+            <div key={cocktail.idDrink} className="flex flex-col gap-5">
+              <div className="space-y-2">
+                <h2 className="text-2xl font-bold tracking-tight sm:text-3xl md:text-4xl">
+                  {cocktail.strDrink}
+                </h2>
+                <p className="text-gray-500 dark:text-gray-400">
+                  {/* {cocktail.strDescription} */}
+                </p>
+              </div>
 
-          <div className="">
-            <Image
-              alt="Image"
-              className="aspect-square object-cover rounded-xl"
-              height="400"
-              src="/strawberry.jpg"
-              width="400"
-            />
-          </div>
+              <div className="">
+                <Image
+                  alt="Image"
+                  className="aspect-square object-cover rounded-xl"
+                  height="400"
+                  src={cocktail.strDrinkThumb!}
+                  width="400"
+                />
+              </div>
 
-          <div className="flex items-end space-x-4">
-            <span className="text-2xl font-bold">$10.00</span>
-            <Button size="sm">Add to Cart</Button>
-          </div>
+              <div className="flex items-end space-x-4">
+                <span className="text-2xl font-bold">
+                  {cocktail.strPrice} $
+                </span>
+                <Button size="sm">Add to Cart</Button>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
 
-        <div className="flex flex-col gap-5 ">
+      {/* <div className="flex flex-col gap-5 ">
           <div className="space-y-2">
             <h2 className="text-2xl font-bold tracking-tight sm:text-3xl md:text-4xl">
               Margarita
@@ -152,33 +159,7 @@ export default function Home() {
             <span className="text-2xl font-bold">$10.00</span>
             <Button size="sm">Add to Cart</Button>
           </div>
-        </div>
-
-        {/* <div className="flex items-center justify-center">
-          <Image
-            alt="Image"
-            className="aspect-square object-cover rounded-xl"
-            height="400"
-            src="/Passion.jpg"
-            width="400"
-          />
-        </div>
-        <div className="flex flex-col justify-between space-y-2">
-          <div className="space-y-2">
-            <h2 className="text-2xl font-bold tracking-tight sm:text-3xl md:text-4xl">
-              Mojito
-            </h2>
-            <p className="text-gray-500 dark:text-gray-400">
-              The Mojito is a refreshing cocktail made with rum, lime juice, and
-              fresh mint.
-            </p>
-          </div>
-          <div className="flex items-end space-x-4">
-            <span className="text-2xl font-bold">$12.00</span>
-            <Button size="sm">Add to Cart</Button>
-          </div>
         </div> */}
-      </div>
     </main>
   );
 }
